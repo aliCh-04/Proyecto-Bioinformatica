a30fb68d4f6491484fdcd8f88f14574000b47cde
david <d.xu.hu@umail.leidenuniv.nl>
2026-04-18 12:20:57 +0200

    modificado el main para aceptar k arbitrario y exportar outputs en un archivo fasta

--- Bio/main.cpp
+++ Bio/main.cpp
@@ -6,39 +6,65 @@
 
 using namespace std;
 
-int main() {
-	ifstream datos("secuencias.txt");
+int main(int argc, char* argv[]) {
 
-	if (!datos.is_open()) {
-		cout << "Error: No se ha podido encontrar o abrir el archivo" << endl;
-		return 1;
-	}
+    if (argc < 4) {
+        cout << "Uso: ./assembler <input> <k> <output>" << endl;
+        cout << "En el Visual Studio, ir a Proyecto -> Propiedades -> Depuración -> Argumentos de comando, y ponerlo ahi." << endl;
+        return 1;
+    }
 
-	string linea;
-	Kmer aux(3);
+    string inputFile = argv[1];
+    int k = stoi(argv[2]);
+    string outputFile = argv[3];
 
-	while (getline(datos, linea)) {
-		aux.procesarLectura(linea);
-	}
-	
-	datos.close();
+    ifstream datos(inputFile);
 
-	cout << "---ESTADO DEL GRAFO DE BRUIJN---" << endl;
-	auto dic = aux.getGrafo();
-	for (const auto& [nodo, arista] : dic) {
-		cout << "[" << nodo << "] ---> ";
-		for (char letra : arista) {
-			cout << letra << " ";
-		}
-		cout << endl;
-	}
+    if (!datos.is_open()) {
+        cout << "Error: No se ha podido encontrar o abrir el archivo" << endl;
+        return 1;
+    }
 
-	cout << "---GENERACION DE CONTIGS---" << endl;
-	vector<string> contigs = generador(aux.getGrafo());
-	cout << "Se han generado un total de : " << contigs.size() << endl;
-	for (int i = 0; i < contigs.size(); i++) {
-		cout << "Contig " << i << ": " << contigs[i] << endl;
-	}
+    string linea;
+    Kmer aux(k);
 
-	return 0;
+    while (getline(datos, linea)) {
+        aux.procesarLectura(linea);
+    }
+
+    datos.close();
+
+    cout << "---ESTADO DEL GRAFO DE BRUIJN---" << endl;
+    auto dic = aux.getGrafo();
+    for (const auto& [nodo, arista] : dic) {
+        cout << "[" << nodo << "] ---> ";
+        for (char letra : arista) {
+            cout << letra << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "---GENERACION DE CONTIGS---" << endl;
+    vector<string> contigs = generador(aux.getGrafo());
+    cout << "Se han generado un total de : " << contigs.size() << endl;
+    for (int i = 0; i < contigs.size(); i++) {
+        cout << "Contig " << i << ": " << contigs[i] << endl;
+    }
+
+    // Guardar contigs en formato FASTA
+    ofstream out(outputFile);
+
+    if (!out.is_open()) {
+        cout << "Error: No se ha podido crear el archivo de salida" << endl;
+        return 1;
+    }
+
+    for (int i = 0; i < contigs.size(); i++) {
+        out << ">contig_" << i << "\n";
+        out << contigs[i] << "\n";
+    }
+
+    out.close();
+
+    return 0;
 }
